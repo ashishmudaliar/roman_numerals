d3491d5c78ceb97ad23ce75dc4554c82ed94dc0c
ashishmudaliar <ashish@qixxit.de>
2019-09-26 20:52:12 +0200

    Add roman numeral for fifty
    - With the addition of "L" for 50, integers upto 89 can be converted to roman numerals
    - Change to using dict to store values for unique numbers and loop over the dict to calculate the roman numeral value for an integer
    - Add test cases for required conversions

--- converter/converter.py
+++ converter/converter.py
@@ -8,18 +8,12 @@ def int_to_roman_converter(numeral):
     return "Please enter valid integers greater than 0 only"
 
 def convert(numeral):
+  key_list = {50 : 'L',40: 'XL',10: 'X',9: 'IX',5: 'V',4: 'IV',1: 'I'}
   result_list = []
-  quotient_ten = numeral // 10
-  result_list.append("X" * quotient_ten)
-  remainder_ten = numeral % 10
-  if remainder_ten == 9:
-    result_list.append("IX")
-  else:
-    quotient_five = remainder_ten // 5
-    remainder_five = remainder_ten % 5
-    if remainder_five == 4:
-      result_list.append("IV")
-    else:
-      result_list.append(("V" * quotient_five) + ("I" * remainder_five))
+  for key,value in key_list.items():
+    count = numeral // key
+    result_list.append(value * count)
+    numeral -= key * count
+    
   return ''.join(result_list)
     
--- test/test_converter_double_digits.py
+++ test/test_converter_double_digits.py
@@ -16,3 +16,21 @@ def test_converter_thirty_five():
 
 def test_converter_thirty_nine():
   assert converter.int_to_roman_converter(39) == "XXXIX"
+
+def test_converter_forty():
+  assert converter.int_to_roman_converter(40) == "XL"
+
+def test_converter_forty_eight():
+  assert converter.int_to_roman_converter(48) == "XLVIII"
+
+def test_converter_fifty():
+  assert converter.int_to_roman_converter(50) == "L"
+
+def test_converter_fifty_nine():
+  assert converter.int_to_roman_converter(59) == "LIX"
+
+def test_converter_seventy():
+  assert converter.int_to_roman_converter(70) == "LXX"
+
+def test_converter_eighty_nine():
+  assert converter.int_to_roman_converter(89) == "LXXXIX"
